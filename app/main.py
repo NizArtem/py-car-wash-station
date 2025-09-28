cd93470c46bb5ee0b65c4f94980f7309c742e5a6
Artem Nizamiiev <upuyayua@gmail.com>
2025-09-28 22:03:12 +0300

    W504 IS NOT HERE

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class CarWashStation:
                  average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self.average_rating = average_rating
+        self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
@@ -24,9 +24,11 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         difference_purity = self.clean_power - car.clean_mark
-        coefficient_of_price = (difference_purity
-                                * self.average_rating
-                                * car.comfort_class)
+        coefficient_of_price = (
+            difference_purity
+            * self.average_rating
+            * car.comfort_class
+        )
         return round(coefficient_of_price / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
